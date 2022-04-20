ddc1db25b6b39fd1e27fec6c7c30705c4c167419
Joaquin Osorio <joaquinosorio55@gmail.com>
2022-04-20 00:10:17 -0300

    navbar

---
+++ pcmidi/src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+//import React from 'react';
+import "./NavBar.css"
+
+function navBar() {
+    return(
+        <div className="menu-container">
+            <ul className="menu">
+                <li href="#">Pc Midi Center</li>
+                <li href="#">Teclados</li>
+                <li href="#">Guitarras</li>
+                <li href="#">Contacto</li>
+            </ul>
+        </div>
+    );
+}
+
+export default navBar;
